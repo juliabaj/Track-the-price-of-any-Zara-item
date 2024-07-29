05daa36c4e8ea6fa1c5698c7b583944c603f4f07
jb <139858294+juliabaj@users.noreply.github.com>
2024-07-29 13:53:30 +0200

    Update zara_price_tracker.py

--- zara_price_tracker.py
+++ zara_price_tracker.py
@@ -17,6 +17,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+
+#creating a header for http request
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
     'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'
